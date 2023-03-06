6b3de01587bddb1c14d6a06bbce876cdd619ae58
ArmSiwatK <arm.siwatk@gmail.com>
2023-03-06 12:48:38 +0700

    Add Dataset

---
+++ data.js
@@ -0,0 +1,44 @@
+export const data1 = [
+    "Rain explosive chaos on your enemies with the Grenade Launcher, and watch them scatter before your might.",
+    "Take careful aim with the Loch-n-Load, and let loose a devastating shot that will leave your enemies reeling.",
+    "With Ali Baba's Wee Booties on your feet, you can cover ground at a breakneck pace and outmaneuver your foes.",
+    "Make your mark on the world with the Bootlegger, leaving a trail of destruction in your wake as you charge through the battlefield.",
+    "Master the art of double-donking with the Loose Cannon, and send your enemies flying through the air with explosive force.",
+    "Defy gravity with the B.A.S.E. Jumper, floating like a leaf on the wind and raining down explosive death from above.",
+    "Unleash a barrage of deadly explosives with the Iron Bomber, and watch as your enemies fall before your unstoppable might."
+]
+
+export const data2 = [
+    "With your Stickybomb Launcher, you can lay traps and control the battlefield with explosive precision.",
+    "Charge fearlessly into battle with the Chargin' Targe and plow through your enemies like a drunken rhinoceros.",
+    "Show your strategic prowess with the Scottish Resistance and set up a network of deadly traps that will make your enemies tremble.",
+    "For a wild ride, strap on the Sticky Jumper and soar through the air with reckless abandon.",
+    "With the Splendid Screen, you can shield bash your way through enemy lines and leave a trail of destruction in your wake.",
+    "The Tide Turner grants you unparalleled mobility and lets you turn the tides of battle in your favor with devastating charges.",
+    "The Quickiebomb Launcher may have a short fuse, but it packs a punch that will blow your enemies away before they even know what hit them.",
+]
+
+export const data3 = [
+    "The Bottle is a classic weapon of choice, break it open and let the shards fly!",
+    "With the Scottish Handshake, you can extend a friendly hand and then bash your opponent's skull in.",
+    "Sizzle your foes into submission with the Frying Pan, showing them who's boss in the kitchen and on the battlefield.",
+    "With the Conscientious Objector, you can make your point without shedding blood, or make your enemies see stars with its blunt edge.",
+    "Show off your patriotic spirit with the Freedom Staff, a symbol of your love for your country and your willingness to fight for it.",
+    "Unleash hell with the Bat Outta Hell, a weapon so fearsome that even the bravest of opponents will cower in terror.",
+    "Capture the moment with the Memory Maker, a camera that doubles as a deadly weapon.",
+    "Satisfy your hunger for victory with the Ham Shank, a meaty weapon that is both delicious and deadly.",
+    "With the Necro Smasher, you can crush your enemies into dust and then raise them back up as your undead servants.",
+    "Keep your teammates safe with the Crossing Guard, a weapon that is both a sign of your dedication to public safety and a deadly club.",
+    "Unleash the power of the netherworld with the Prinny Machete, a weapon that channels the spirits of the dead.",
+    "Take down your enemies in style with the Saxxy, an elegant and deadly weapon that is also a symbol of your creative genius.",
+    "Strike fear into the hearts of your enemies with the Golden Frying Pan, a weapon that shines with the radiance of your victory.",
+    "Claim your foes' heads with the Eyelander, a weapon that grows stronger with each kill.",
+    "Take on the Horseless Headless Horsemann himself with the Headtaker, a weapon that is as deadly as it is iconic.",
+    "Swing for the green with Nessie's Nine Iron, a weapon that is as precise as it is powerful.",
+    "Take no prisoners with the Pain Train, a weapon that leaves a trail of destruction in its wake.",
+    "Show off your warrior spirit with the Scotsman's Skullcutter, a weapon that is as intimidating as it is deadly.",
+    "Channel the fury of the Highlands with the Claidheamh Mòr, a weapon that is as powerful as it is elegant.",
+    "Blow your enemies sky high with the Ullapool Caber, a weapon that sacrifices itself to deal massive damage.",
+    "Bring honor to your clan with the Half-Zatoichi, a weapon that demands satisfaction and deals death.",
+    "Persuade your enemies to join your side with the Persian Persuader, a weapon that can turn the tide of battle with its explosive power."
+]
--- readme.md
+++ readme.md
@@ -10,3 +10,25 @@ For this project, you will build a message generator program. Every time a user 
 * Use Git version control
 * Use command line
 * Develop locally on your computer
+
+## Project Plan:
+
+Just a random message generator written in JavaScript. Yes, that is half the point of the project – the other half is using GitHub here.
+
+&nbsp;
+
+# 2 – Come Up with a Topic
+
+### Decide on what types of messages you want your program to output.
+
+Think about what kinds of messages you’d like your program to output. Is it a horoscope generator? A fortune teller? An inspiring phrase? Knock-knock jokes? Here’s an example of what the output of a horoscope generator could look like:
+
+`Your sign is sun.`
+
+`You are having good luck. `
+
+`You should: "trust no one"`
+
+Remember, to make your program truly random the message should really be made up of multiple strings that are output together.
+
+Think about what parts of the string could stay the same each time and what parts could change. In the output above, what do you think are the pieces that change each time?
